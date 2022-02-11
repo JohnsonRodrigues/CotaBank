9e6e351f029ca9e936291c0fa685a7b1f43beb06
Johnson Rodrigues <johnsonrodrigues19@gmail.com>
2022-02-11 09:20:24 -0300

    create reversal

--- src/CotaBank.php
+++ src/CotaBank.php
@@ -6,11 +6,13 @@ class CotaBank
 {
     private Connection $connection;
     private Charge $charge;
+    private Reversal $reversal;
 
     public function __construct(string $user, string $password, string $environment)
     {
         $this->connection = new Connection($user, $password, $environment);
         $this->charge = new Charge($this->connection);
+        $this->reversal = new Reversal($this->connection);
     }
 
     public function getCharge(): Charge
@@ -18,7 +20,8 @@ class CotaBank
         return $this->charge;
     }
 
-
-
-
+    public function getReversal(): Reversal
+    {
+        return $this->reversal;
+    }
 }
